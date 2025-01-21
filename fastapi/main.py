878de39a999122936a2d7cee22ef42fb01d403bd
jaemin.lee00 <jemin0994@gmail.com>
2025-01-21 11:29:52 +0900

    File name Change

--- fastapi/main.py
+++ fastapi/main.py
@@ -29,14 +29,6 @@ app.mount(
 async def serve_index():
     return FileResponse("C:/Users/Admin/Documents/GitHub/IPTV_FINAL/fastapi/static/build/index.html")
 
-# CORS 설정
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
